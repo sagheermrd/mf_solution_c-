2710cd879d1ad757035288d642b6d005909a3d13
Sagheer <>
2020-04-12 16:35:39 +0100

    add logic to take input from console and return repeated words

--- MF/Program.cs
+++ MF/Program.cs
@@ -6,7 +6,15 @@ namespace MF
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Enter input string");
+            var input = Console.ReadLine();
+        
+            var repeatedWords = new Utility().RepeatedWords(input);
+            foreach (var word in repeatedWords)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadKey();
         }
     }
 }
